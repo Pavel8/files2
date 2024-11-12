3303829717c6230be88cebf8a6ed3a3929c8986b
Pavel8 <pvlkadlec@gmail.com>
2024-11-12 21:24:50 +0100

    Update objekty_start.py

--- objekty_start.py
+++ objekty_start.py
@@ -12,3 +12,30 @@ print(t2.tvar)
 
 
 class city:
+    def __init__(self, name:str, region, country, people:int, ZIP):
+        self.name = name
+        self.region = region
+        self.country = country
+        self.people = people
+        self.ZIP = ZIP
+
+    def __str__(self):
+        return f"name:{self.name}, region:{self.region}, country:{self.country}, people:{self.people}, ZIP:{self.ZIP}"
+
+    @staticmethod
+    def city_create():
+        name = input(f"Pridej jmeno:")
+        region = input("Pridej region: ")
+        country = input("Pridej country: ")
+        people = int(input("Pridej pocet obyvatel: "))  # Convert to integer
+        ZIP = input("Pridej ZIP kod: ")
+        return city(name,region,country,people,ZIP)
+
+c2 = city.city_create()
+
+c1 = city("Brno","JM_kraj","CZ", 400000, [60200, 63800])
+
+
+print (c1)
+
+
